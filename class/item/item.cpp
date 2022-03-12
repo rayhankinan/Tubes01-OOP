c0436d59cca7844512aff3181c5ea0c45c8afcac
rayhankinan <rayhankinan@gmail.com>
2022-03-13 05:21:50 +0700

    Menambahkan item interface

---
+++ class/item/item.cpp
@@ -0,0 +1,117 @@
+#include "itemInterface.hpp"
+
+int Item::numOfItem = 0;
+
+Item::Item() {
+    this->ID = 0;
+    this->name = "";
+    this->type = "";
+    this->quantity = 0;
+
+    Item::numOfItem++;
+}
+
+Item::Item(int ID, string name, string type, int quantity) {
+    this->ID = ID;
+    this->name = name;
+    this->type = type;
+    this->quantity = quantity;
+
+    Item::numOfItem++;
+}
+
+Item::Item(const Item& I) {
+    this->ID = I.ID;
+    this->name = I.name;
+    this->type = I.type;
+    this->quantity = quantity;
+
+    Item::numOfItem++;
+}
+
+Item::~Item() {
+    Item::numOfItem--;
+}
+
+Item& Item::operator=(const Item& I) {
+    this->ID = I.ID;
+    this->name = I.name;
+    this->type = I.type;
+    this->quantity = quantity;
+
+    return *this;
+}
+
+int Item::getID() const {
+    return this->ID;
+}
+
+void Item::setID(int ID) {
+    this->ID = ID;
+}
+
+string Item::getName() const {
+    return this->name;
+}
+
+void Item::setName(string name) {
+    this->name = name;
+}
+
+string Item::getType() const {
+    return this->type;
+}
+
+void Item::setType(string type) {
+    this->type = type;
+}
+
+int Item::getQuantity() const {
+    return this->quantity;
+}
+
+Item& Item::operator++() {
+    ++this->quantity;
+    return *this;
+}
+
+Item& Item::operator--() {
+    --this->quantity;
+    return *this;
+}
+
+Item Item::operator++(int) {
+    this->quantity++;
+
+    return *this;
+}
+
+Item Item::operator--(int) {
+    this->quantity--;
+
+    return *this;
+}
+
+int Item::getNumOfItem() {
+    return Item::numOfItem;
+}
+
+istream& operator>>(istream& is, Item& I) {
+    I.input(is);
+
+    return is;
+}
+
+ostream& operator<<(ostream& os, const Item& I) {
+    I.output(os);
+
+    return os;
+}
+
+void Item::input(istream& is) {
+    is >> this->ID >> this->name >> this->type >> this->quantity;
+}
+
+void Item::output(ostream& os) const {
+    os << this->ID << " " << this->name << " " << this->type << " " << this->quantity;
+}
---
+++ class/item/itemInterface.hpp
@@ -0,0 +1,48 @@
+#ifndef ITEM_INTERFACE_HPP
+#define ITEM_INTERFACE_HPP
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Item {
+    private:
+        static int numOfItem;
+
+        int ID;
+        string name;
+        string type;
+        int quantity;
+
+    public:
+        Item(); // default ctor
+        Item(int, string, string, int); // user-defined ctor
+        Item(const Item&); // cctor
+        ~Item(); // dtor
+        Item& operator=(const Item&); // assignment operator
+
+        int getID() const; // ID getter
+        void setID(int); // ID setter
+
+        string getName() const; // name getter
+        void setName(string); // name setter
+
+        string getType() const; // type getter
+        void setType(string); // type setter
+
+        int getQuantity() const; // quantity getter
+        Item& operator++(); // quantity prefix increment
+        Item& operator--(); // quantity prefix decrement
+        Item operator++(int); // quantity postfix increment
+        Item operator--(int); // quantity postfix decrement
+
+        static int getNumOfItem(); // numOfItem getter
+
+        friend istream& operator>>(istream&, Item&); // console input stream
+        friend ostream& operator<<(ostream&, const Item&); // console output stream
+
+        virtual void input(istream&); // console input method (delegated from stream)
+        virtual void output(ostream&) const; // console output method (delegated from stream)
+};
+
+#endif
