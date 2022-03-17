e7586c517a0682d3d2a262cb02fba67b78adc73f
Sarah Azka Arief <sarah_azka@yahoo.com>
2022-03-18 01:24:35 +0700

    implement recipe class

---
+++ class/crafting/recipe.cpp
@@ -0,0 +1,85 @@
+#include "recipeInterface.hpp"
+
+
+// default ctor (max-sized configRecipe w/ empty contents)
+Recipe::Recipe() {
+    this->row = 3;
+    this->col = 3;
+    this->productQty = 0;
+    this->productName = "-";
+    this->configRecipe = Matrix<string>(3, 3);
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){
+            this->setConfigElmt(i, j, "-");
+        }
+    }
+}
+
+// cctor
+Recipe::Recipe(const Recipe& r) {
+    this->row = r.row;
+    this->col = r.col;
+    this->productQty = r.productQty;
+    this->productName = r.productName;
+    this->configRecipe = Matrix<string>(r.row, r.col);
+    for (int i = 0; i < r.row; i++){
+        for (int j = 0; j < r.col; j++){
+            string elmt = r.configRecipe(i, j);
+            this->setConfigElmt(i, j, elmt);
+        }
+    }
+}
+
+// parameterized ctor (configRecipe contents not initialized)
+Recipe::Recipe(int row, int col, int productQty, string productName) {
+    this->row = row;
+    this->col = col;
+    this->productQty = productQty;
+    this->productName = productName;
+    this->configRecipe = Matrix<string>(row, col);
+}
+
+// dtor
+Recipe::~Recipe() {
+    this->configRecipe.~Matrix();
+}
+
+// getter
+int Recipe::getRow() const {
+    return this->row;
+}
+
+int Recipe::getCol() const {
+    return this->col;
+}
+
+int Recipe::getProductQty() const {
+    return this->productQty;
+}
+
+string Recipe::getProductName() const {
+    return this->productName;
+}
+
+// setter
+void Recipe::setRow(int row) {
+    this->row = row;
+}
+
+void Recipe::setCol(int col) {
+    this->col = col;
+}
+
+void Recipe::setProductQty(int productQty) {
+    this->productQty = productQty;
+}
+
+void Recipe::setProductName(string productName) {
+    this->productName = productName;
+}
+
+void Recipe::setConfigElmt(int row, int col, string elmt) {
+    if (0 <= row && row < this->row && 0 <= col && col < this->col){
+        this->configRecipe(row, col) = elmt;
+    }
+}
---
+++ class/crafting/recipeInterface.hpp
@@ -0,0 +1,40 @@
+#ifndef RECIPE_INTERFACE_HPP
+#define RECIPE_INTERFACE_HPP
+
+#include "../collection/matrixInterface.hpp"
+#include "../item/nonToolInterface.hpp"
+#include "../item/itemInterface.hpp"
+#include "../item/toolInterface.hpp"
+
+class Recipe {
+private:
+    // attributes
+    int row;
+    int col;
+    int productQty;
+    string productName;
+    Matrix<string> configRecipe = Matrix<string>(row, col);
+
+public:
+    Recipe(); // default ctor
+    Recipe(const Recipe&); // cctor
+    Recipe(int, int, int, string); // parameterized ctor
+    ~Recipe(); // dtor
+
+    // getter
+    int getRow() const;
+    int getCol() const;
+    int getProductQty() const;
+    string getProductName() const;
+
+    // setter
+    void setRow(int);
+    void setCol(int);
+    void setProductQty(int);
+    void setProductName(string);
+    void setConfigElmt(int, int, string);
+
+};
+
+
+#endif
