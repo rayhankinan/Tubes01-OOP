9c72afa3e8c1486ee2c5cd86500534de8aa58abc
Adiyansa Prasetya Wicaksana <adiyansawicaksana@gmail.com>
2022-03-25 00:07:42 +0700

    semoga final aamiin yrb

--- class/consoleIO/command.cpp
+++ class/consoleIO/command.cpp
@@ -14,7 +14,6 @@ int Command::getNumOfCommand()
 
 void Command::SHOW(Inventory &inv, CraftingTable& table)
 {
-  cout << "SHOW" << endl;
   Command::numOfCommand++;
   table.show();
   inv.show();
@@ -22,52 +21,42 @@ void Command::SHOW(Inventory &inv, CraftingTable& table)
 
 void Command::DISCARD(Inventory &inv, string slotID, int qty)
 {
-  cout << "DISCARD " << slotID << " " << qty << endl;
   Command::numOfCommand++;
   inv.discard(slotID, qty);
 }
 
 void Command::MOVE(Inventory &inv, string slotSrc, int qty, string slotTarget)
 {
-  cout << "MOVE" << " " << slotSrc << " " << qty << " " << slotTarget << endl;
   Command::numOfCommand++;
   inv.moveInInventory(slotSrc, qty, slotTarget);
 }
 
 void Command::MOVE(CraftingTable &table, Inventory &inv, string slotCraft, int n, string slotInv)
 {
-  cout << "MOVE" << " " << slotCraft << " " << n << " " << slotInv << endl;
   Command::numOfCommand++;
   table.move(inv, slotCraft, n, slotInv);
 }
 
 void Command::MOVE(CraftingTable &table, Inventory &inv, string slotInv, int n, vector<string> slotCrafts)
 {
-  cout << "MOVE" << " " << slotInv << " " << n << " ";
-  for (int i = 0; i < n; i++) {
-    cout << slotCrafts[i] << " ";
-  } 
-  cout << endl;
+  Command::numOfCommand++;
   table.move(inv, slotInv, n, slotCrafts);
 }
 
 void Command::USE(Inventory &inv, string slotID)
 {
-  cout << "USE " << slotID << endl;
   Command::numOfCommand++;
   inv.use(slotID);
 }
 
 void Command::CRAFT(CraftingTable &table, Inventory& inv)
 {
-  cout << "CRAFT" << endl;
   Command::numOfCommand++;
   table.craft(inv);
 }
 
 void Command::EXPORT(Inventory &inv, string fileName)
 {
-  cout << "EXPORT " << fileName << endl;
   Command::numOfCommand++;
   inv.EXPORT(fileName);
 }
@@ -75,7 +64,6 @@ void Command::EXPORT(Inventory &inv, string fileName)
 
 void Command::GIVE(Inventory &inv, string name, int qty)
 {
-  cout << "GIVE " << name << " " << qty << endl;
   Command::numOfCommand++;
   inv.give(name, qty);
 }
